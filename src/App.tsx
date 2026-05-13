14285313921cdc04713c29321fd1fbbd3c1e5689
Nazar <tomchuknazarnt@gmail.com>
2026-05-13 16:26:19 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <div className="text-red-500 text-4xl">
+        Test Tailwind
+      </div>
+      <h1>Eurostok</h1>
+    </div>
+  );
+}
+
+export default App;
